57be9c58125ed560d98d77ff0a7e0f08119e44c1
Felix.Gilioli <felix.gilioli@senior.com.br>
2021-05-20 09:46:01 -0300

    #1 - workstation

--- src/main/java/br/com/praguedo/chatpodre/ChatSocket.java
+++ src/main/java/br/com/praguedo/chatpodre/ChatSocket.java
@@ -8,47 +8,35 @@ import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
-import javax.websocket.SendHandler;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/chat/{username}")
+@ServerEndpoint("/chat/{workspace}/{username}")
 @ApplicationScoped
 public class ChatSocket {
 
-    private final Map<String, Session> sessions = new ConcurrentHashMap<>();
+    private final Map<String, User> sessionByWorkspace = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("username") String username) {
-        this.sessions.put(username, session);
+    public void onOpen(Session session, @PathParam("workspace") String workspace, @PathParam("username") String username) {
+        this.sessionByWorkspace.put(workspace, new User(username, session));
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("username") String username) {
-        this.sessions.remove(username);
+    public void onClose(Session session, @PathParam("workspace") String workspace, @PathParam("username") String username) {
+        this.sessionByWorkspace.remove(workspace);
     }
 
     @OnError
-    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
-        this.sessions.remove(username);
+    public void onError(Session session, @PathParam("workspace") String workspace, @PathParam("username") String username, Throwable throwable) {
+        this.sessionByWorkspace.remove(workspace);
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("username") String username) {
-        broadcast(username + ": " + message);
+    public void onMessage(String message, @PathParam("workspace") String workspace, @PathParam("username") String username) {
+        MessageBroadcast.send(this.sessionByWorkspace, new Message(message, workspace, username));
     }
 
-    private void broadcast(String message) {
-        this.sessions.values().forEach(s -> s.getAsyncRemote().sendObject(message, this.handlerErrorMessage()));
-    }
-
-    private SendHandler handlerErrorMessage() {
-        return result -> {
-            if (result.getException() != null) {
-                result.getException().printStackTrace();
-            }
-        };
-    }
 
 }
